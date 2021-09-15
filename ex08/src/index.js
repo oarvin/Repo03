9f16b6164d1cad409eec8405cd663b5dab1bee7a
oarvin <oprasicarvin@gmail.com>
2021-09-15 19:23:05 +0200

    Exercise

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,6 +1,8 @@
 function main(numOne,numTwo) { 
     // Only change code below this line
 
+    var numOne = numOne;
+    var numTwo = numTwo;
     var sum = numOne + numTwo;
 
     // Only change code above this line
