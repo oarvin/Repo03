4adb7159cd21ae3856cd873f57cbc792228133a3
oarvin <oprasicarvin@gmail.com>
2021-09-15 18:53:11 +0200

    Exercise 7

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+function main() { 
+    // Only change code below this line
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assignments
+    var studlyCapVar = 10;
+    var properCamelCase = "A String";
+    var titleCaseOver = 9000;
+    
+    // Only change code above this line
+    
+    return {
+        studlyCapVar, properCamelCase, titleCaseOver
+    }
+    
+
+} 
+console.log(main());
+module.exports = main;
