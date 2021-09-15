b366bd58a48d3e3f3cc01b89cf5ef1a788620997
oarvin <oprasicarvin@gmail.com>
2021-09-15 18:07:55 +0200

    Ex 01 and 02

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+function main() { 
+    // Comment type line
+
+    /* Comment type block 
+    Another way of comment */
+} 
+main(); 
+
+module.exports = main;
---
+++ ex02/src/index.js
@@ -0,0 +1,6 @@
+function main() { 
+    console.log('Hellow World!'); 
+} 
+main(); 
+
+module.exports = main;
