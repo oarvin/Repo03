7fcb92d3d4ec95c916c8f3dc936e3b38df0c39ed
oarvin <oprasicarvin@gmail.com>
2021-09-16 22:24:04 +0200

    Exercise

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -2,5 +2,6 @@ function main() {
     console.log('Hellow World!'); 
 } 
 main(); 
-
+// Missing comment is here now in position
+/* Another comment is here for the same reason */
 module.exports = main;
